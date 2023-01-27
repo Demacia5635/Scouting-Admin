5c96edca917ddfe3c5b5f9716284e4df488fc386
greendragon345 <yehonatan.hacham@gmail.com>
2023-01-27 12:51:35 +0200

    get data from firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,31 @@
 // create a home page component
+import { collection, doc, getDoc } from 'firebase/firestore/lite';
 import React from 'react';
+import { firebase, firestore } from '../utils/firebase';
+
+function GetSeasonsList(){
+    let Seasons: Array<HTMLDivElement>;
+    let i:number;
+    i=0;
+    let isfalied:boolean
+    isfalied = false
+    while(!isfalied)
+    {
+        try {
+            const getfieldvalue = async () => {
+                let docsnap = await getDoc(doc(firestore,"seasons",(2023+i).toString()))
+                return (docsnap.get("name"))
+                
+            }
+
+        } catch (error) {
+            isfalied = true
+        }
+
+    }
+
+}
+
 
 export const Home = () => {
     return (
