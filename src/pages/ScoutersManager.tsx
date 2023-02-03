6ec3860de9a69312a0b3f20a9ab1b9c65f892511
Greninja <BobbyBoten@gmail.com>
2023-02-03 04:02:47 +0200

    scouter manager added temporary placeholder for getseasons

--- src/pages/ScoutersManager.tsx
+++ src/pages/ScoutersManager.tsx
@@ -1,17 +1,22 @@
 import { useEffect } from "react";
+import { Select } from "../components/html/Select";
+import { getfieldvalue, getSeasons } from "../utils/firebase";
 import { resetSeason } from "../utils/season-handler";
-import { DropDownMenu } from "../components/DropDownMenu";
+
+
+
+
 
 export const ScoutersManager = () => {
 
     useEffect(() => {
         resetSeason();
     }, []);
-
     return (
         <div>
             <h1>Scouters Manager</h1>
-            <DropDownMenu/>
+            {/* <Select id="teamselectmenu" options={[{"paso"}]}></Select> */}
+
         </div>
     );
 }
