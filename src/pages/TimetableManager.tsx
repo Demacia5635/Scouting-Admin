136bf63f3ed0d8309f0ace7c02795f929594432a
jhony <yehonatan.hacham@gmail.com>
2023-02-13 19:05:56 +0200

    timetable manager branch

--- src/pages/TimetableManager.tsx
+++ src/pages/TimetableManager.tsx
@@ -1,9 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { resetSeason } from "../utils/season-handler";
+import { Buffer } from 'buffer'
 
 export const TimetableManager = () => {
-
+    const [events, setEvents] = useState<Event[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
     useEffect(() => {
+        setLoading(true);
+        setError(null);
+        const targetUrl = `http://localhost:3000/frcapi/v3.0/2023/events?districtCode=ISR`;
+        const myHeaders = new Headers();
+        myHeaders.append("If-Modified-Since", "");
+
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        } as RequestInit;
+
+        fetch(targetUrl, requestOptions)
+            .then(response => response.json())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+        console.log(events)
         resetSeason();
     }, []);
 
